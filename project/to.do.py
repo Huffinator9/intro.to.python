e847120f1c2a09e272e34200ff1a5a9df7414215
Huffinator9 <huffinator@nerdwatchtower.com>
2025-08-04 13:58:35 -0700

    started to do app

---
+++ project/to.do.py
@@ -0,0 +1,10 @@
+# Coding Temple Intro to Python Module Project
+# To Do CLI App
+
+# Greeting
+user = input("Please provide your name: ")
+
+print (f"Hello, {user}! What would you like to do today?")
+
+try:
+    task = 
